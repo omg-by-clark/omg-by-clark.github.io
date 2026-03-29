1aff424d499d79890eecb0dc57ccd3d6d9497e33
35727 <35727854@qq.com>
2026-03-29 01:42:25 -0700

    v2.6.4：删除没有用的文件

--- home-h1suprise.js
+++
@@ -1,7 +0,0 @@
-// home-h1suprise.js
-const mainTitle = document.getElementById('main-title');
-mainTitle.addEventListener('click', () => {
-    const lang = localStorage.getItem('lang') || 'zh';
-    const languChange = lang == 'zh' ? '恭喜你看到了这个隐藏彩蛋！' : 'Congratulations on finding this hidden Easter egg!';
-    alert(languChange);
-});
